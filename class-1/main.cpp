86c9ab427c9392a742f5dbdb137b946f0ab3aee7
Arpit Jaswal <arpitjaswal@Arpits-MacBook-Air.local>
2025-08-22 02:47:36 +0530

    pushing yesterday's changes

---
+++ class-1/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main(){
+
+    cout<<"testing testing !"<<endl;
+    cout<<test<<endl;
+    return 0;
+}
--- class-1/main.js
+++ class-1/main.js
@@ -1,1 +1,22 @@
-console.log("testing")
+
+// let fname = 'lodu'
+// let lname = "chand"
+// let gender = 'female'
+
+// if(gender==='female'){
+//     console.log("kaisi hai, "+fname+" "+lname);
+// }else{
+//     console.log("kaisa hai, "+fname+" "+lname);
+// }
+// // console.log("hey, how are you doing, "+fname+" "+lname);
+
+
+// for(let i=0;i<1000;i++){
+//     console.log("lodu chand"+i);
+// }
+
+
+let test = 2
+test=1
+console.log(test)
+
